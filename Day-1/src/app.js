4b664151613c547922dbcdfe88cbb1580e59867b
Joohhnnyyy <johnsonansh32@gmail.com>
2026-03-23 04:26:28 +0530

    update

--- Day-1/src/app.js
+++ Day-1/src/app.js
@@ -5,4 +5,12 @@ const app = express();//creating server instance
 app.get('/',(req,res) =>{
   res.send('Hello World');
 })
+
+app.get('/about',(req,res) =>{
+  res.send('This is the about page');
+});
+
+app.get('/contact',(req,res) =>{
+  res.send('This is the contact page');
+});
 module.exports = app;
